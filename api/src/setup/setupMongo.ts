20dfbbcf79bb1aee80e2473f944e8146c93f2a28
owsky <nick.bertocco@gmail.com>
2021-05-30 09:23:48 +0000

    Moved DB connection string to .env file

--- api/src/setup/setupMongo.ts
+++ api/src/setup/setupMongo.ts
@@ -1,27 +1,29 @@
 import mongoose = require('mongoose')
 import { exit } from 'process'
 import logger from '../logger/'
-import { newUser, getUserByEmail, setAdmin } from '../mongo/user'
+import { getUserByEmail, newUser, setAdmin } from '../mongo/user'
 
 export async function setupDB() {
   // Connect to mongodb and launch the HTTP server trough Express
-  try {
-    const db = await mongoose.connect('mongodb://localhost:27017/connectfourdb', {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-      useUnifiedTopology: true
-    })
-    logger.info("Connected to MongoDB")
-    const doc = await getUserByEmail("admin@connectfour.it")
-    if (!doc) {
-      logger.info("Creating admin user")
-      const admin = await newUser('admin', 'admin@connectfour.it', 'admin')
-      await setAdmin(admin)
+  if (process.env.DB_CONNECTION_STRING) {
+    try {
+      const db = await mongoose.connect(process.env.DB_CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+      })
+      logger.info("Connected to MongoDB")
+      const doc = await getUserByEmail("admin@connectfour.it")
+      if (!doc) {
+        logger.info("Creating admin user")
+        const admin = await newUser('admin', 'admin@connectfour.it', 'admin')
+        await setAdmin(admin)
+      }
+      return db
+    } catch (err) {
+      logger.error(`Error Occurred during initialization: ${err}`)
+      exit(1)
     }
-    return db
-  } catch (err) {
-    logger.error(`Error Occurred during initialization: ${err}`)
-    exit(1)
   }
 }
