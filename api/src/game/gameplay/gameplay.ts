93f0bf37ee42b9b433bda4b27b6c19925da0b5fc
owsky <nick.bertocco@gmail.com>
2021-06-07 11:17:11 +0200

    Cleaned code a lil

--- api/src/game/gameplay/gameplay.ts
+++ api/src/game/gameplay/gameplay.ts
@@ -8,6 +8,7 @@ import { redisClient as redis } from '../../setup/setupRedis'
 import freePortFinder from '../../utils/freePortFinder'
 import UnmatchedPlayer from '../matchmaking/UnmatchedPlayer'
 import Player from '../Player'
+import { matchCallback } from '../socket/matchSocket'
 import { Match } from './Match'
 import MoveResult from './MoveResult'
 
@@ -41,50 +42,7 @@ export async function gameStart(p1: UnmatchedPlayer, p2: UnmatchedPlayer) {
     p2.ws.broadcast.emit('playing', p2.player.username, port)
 
     logger.info(`Started a new match between ${player1.username} and ${player2.username}`)
-    io.on('connection', socket => {
-      if (socket.request['user._id'] == player1.id || socket.request['user._id'] == player2.id)
-        socket.join('playersChat')
-      else
-        socket.join('observersChat')
-
-      socket.on('message', message => {
-        if (socket.request['user._id'] == player1.id || socket.request['user._id'] == player2.id)
-          socket.to('playersChat').to('observersChat').emit('message', message)
-        else
-          socket.to('observersChat').emit('message', message)
-      })
-
-      socket.on('dot', (column: number) => {
-        const user: User = socket.request['user']
-        const moveResult = match.addDot(column, user._id)
-        if (moveResult.accepted) {
-          socket.broadcast.emit('dot', column)
-          if (moveResult.matchResult) {
-            io.emit('winner', `Player ${socket.request['user.username']} has won the match!`)
-            p1.ws.broadcast.emit('stoppedPlaying', p1.player.username)
-            p2.ws.broadcast.emit('stoppedPlaying', p2.player.username)
-            io.disconnectSockets()
-          }
-        } else {
-          io.emit('playerMoveRejection', column, user.username)
-        }
-      })
-
-      socket.on('disconnect', async (reason) => {
-        try {
-          const username = await hgetAsync('users', socket.id)
-          if (username === player1.username) {
-            endMatch({ winner: player2.username, loser: username })
-            io.emit('playerDisconnected', username, reason)
-          } else if (username === player2.username) {
-            endMatch({ winner: player1.username, loser: username })
-            io.emit('playerDisconnected', username, reason)
-          }
-        } catch (err) {
-          logger.prettyError(err)
-        }
-      })
-    })
+    io.on('connection', socket => { matchCallback(match, p1, p2, io, socket) })
   } else {
     logger.error("Couldn't find a free port")
   }
