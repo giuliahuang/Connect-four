57dc2c2ed2ce16058f344940b7663cce16961c16
GiuliaHuang <61154039+giuliahuang@users.noreply.github.com>
2021-06-26 02:06:42 +0200

    adjust username color in chat

--- client/src/app/components/game/gamechat/gamechat.component.ts
+++ client/src/app/components/game/gamechat/gamechat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, HostBinding, OnChanges, SimpleChanges } from '@angular/core'
 import { GamesocketService } from 'src/app/services/gamesocket.service'
+import { SocketioService } from 'src/app/services/socketio.service'
 import { UserHttpService } from 'src/app/services/user-http.service'
 
 @Component({
@@ -10,18 +11,30 @@ import { UserHttpService } from 'src/app/services/user-http.service'
 export class GamechatComponent implements OnInit {
   @Input() playerColor: string = ''
   otherPlayerColor: string = ''
-  username: string
+  username: string = ''
+  otherUsername: string = ''
+  isObserver: boolean
   messageText: String = ''
   messageArray: Array<{ user: String, message: String }> = [];
 
   constructor(
     private gamesocketService: GamesocketService,
-    private userHttpService: UserHttpService) {
-    this.username = this.userHttpService.username
+    private userHttpService: UserHttpService,
+    private socketIoService: SocketioService
+    ) {
+    this.isObserver=this.socketIoService.isObserver
+    if(!this.isObserver){
+      this.username = this.userHttpService.username
+    }
+    else{
+      this.username = this.socketIoService.currentPlayer
+    }
+    this.otherUsername = this.socketIoService.otherPlayer
   }
 
   ngOnInit(): void {
     this.receiveMessage()
+    console.log(this.playerColor)
     if (this.playerColor === 'red')
       this.otherPlayerColor = 'blue'
     else
@@ -31,13 +44,15 @@ export class GamechatComponent implements OnInit {
   sendMessage() {
     if (this.messageText.trim().length !== 0 && this.messageText.length <= 150) {
       this.gamesocketService.sendMessage(this.messageText)
-      this.messageArray.push({ user: this.username, message: this.messageText })
     }
     this.messageText = ''
   }
 
   receiveMessage() {
+    console.log("START RECEIVING")
     this.gamesocketService.receiveMessage().subscribe((data: any) => {
+      
+      console.log("RECEIVED")
       this.messageArray.push({ user: data.player, message: data.message })
       this.messageText = ''
     })
